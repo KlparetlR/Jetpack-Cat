41d18b3c344cca8b9ac93ac69e9a2d6b6e683fad
KlparetlR <105189905+KlparetlR@users.noreply.github.com>
2022-11-25 20:14:08 +0800

    3.5(p1)

--- 汉化本体（把里面的文件拖入已安装的Jetpack Cat或服务器）/3.4.2/kubejs/server_scripts/drop.js
+++
@@ -1,7 +0,0 @@
-// onEvent('item.right_click', (event) => {
-//   if (event.item.id == 'xpcoins:platinum_coin' || event.item.id == 'xpcoins:coin_press' || event.item.id == 'xpcoins:golden_coin' ) {
-//     event.cancel()
-//     event.item.count--;
-//     event.player.tell('此物品已删除，抱歉 ¯\\_(ツ)_/¯');
-//   }
-// });
