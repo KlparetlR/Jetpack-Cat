21d961fdc26f7d16c4ba68ef58c1107ca81e99c1
KlparetlR <105189905+KlparetlR@users.noreply.github.com>
2022-12-17 12:06:24 +0800

    1.5.0.0-3.6-p2

---
+++ 汉化本体（把里面的文件拖入已安装的Jetpack Cat或服务器）/3.6/汉化本体（把里面的文件拖入已安装的Jetpack Cat或服务器）/kubejs/server_scripts/script.js
@@ -0,0 +1,20 @@
+// priority: 0
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('Hello, World! (每次重新加载服务器资源时，您都会看到这一行)')
+
+onEvent('recipes', event => {
+	// Change recipes here
+})
+
+onEvent('item.tags', event => {
+	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
+	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+
+	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
+	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+})
